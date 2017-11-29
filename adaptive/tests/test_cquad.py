662882c9930ccdb25eabc6de9b0e87bc058b3e3b
Bas Nijholt <basnijholt@gmail.com>
2017-11-29 15:59:10 +0100

    cwg's code to commit 4d84d696d59f99b6fb29519cafc7a9082700dfe9

--- adaptive/tests/test_cquad.py
+++ adaptive/tests/test_cquad.py
@@ -50,7 +50,8 @@ def test_machine_precision():
 
 
 def test_machine_precision2():
-    f, a, b, tol = [f63, 0, 1, 1e-10]
+    from functools import partial
+    f, a, b, tol = [partial(f63, u=0.987654321, e=0.45), 0, 1, 1e-10]
     igral, err, nr_points, ivals = algorithm_4(f, a, b, tol)
 
     learner = run_integrator_learner(f, a, b, tol, nr_points)
