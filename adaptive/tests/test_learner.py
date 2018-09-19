62e469543f31626d0872f4247fb4be58f81bcfad
Bas Nijholt <basnijholt@gmail.com>
2018-09-19 14:50:02 +0200

    add test that makes sure that the 'AverageLearner' only returns new points

--- adaptive/tests/test_learner.py
+++ adaptive/tests/test_learner.py
@@ -560,6 +560,22 @@ def test_small_deviations():
             break
 
 
+def test_average_learner_only_returns_new_points():
+    learner = AverageLearner(lambda x: x, atol=None, rtol=0.01)
+
+    # Only tell it n = 5...10
+    for i in range(5, 10):
+        learner.tell(i, 1)
+
+    learner.tell(0, None)  # This means it shouldn't return 0 anymore
+
+    assert learner.ask(1)[0][0] == 1
+    assert learner.ask(1)[0][0] == 2
+    assert learner.ask(1)[0][0] == 3
+    assert learner.ask(1)[0][0] == 4
+    assert learner.ask(1)[0][0] == 10
+
+
 @pytest.mark.xfail
 @run_with(Learner1D, Learner2D, LearnerND)
 def test_convergence_for_arbitrary_ordering(learner_type, f, learner_kwargs):
